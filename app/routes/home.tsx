bcdde57af3886e8faa54b3f06160fd2117070a7e
pavan02m <pavan.metkari02@gmail.com>
2025-12-11 22:22:34 +0530

    Implemented delete functionality

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import Navbar from "~/components/Navbar";
 import {resumes} from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
 import {usePuterStore} from "~/lib/puter";
-import {useLocation, useNavigate} from "react-router";
-import {useEffect} from "react";
+import {Link, useLocation, useNavigate} from "react-router";
+import {useEffect, useState} from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -15,14 +15,33 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
 
-    const {auth} = usePuterStore();
+    const {auth, kv} = usePuterStore();
+    const[loadingResumes, setLoadingResumes] = useState<boolean>(false);
+    const[resumes, setResumes] = useState<Resume[]>([]);
     const navigate = useNavigate();
 
     useEffect(() => {
         console.log("Home page");
         console.log(auth.isAuthenticated);
         if(!auth.isAuthenticated) navigate('/auth?next=/');
-    },[auth.isAuthenticated])
+    },[auth.isAuthenticated]);
+
+    useEffect(() => {
+        const loadResumes = async () => {
+            setLoadingResumes(true);
+
+            const resumes = (await kv.list('resume:*', true)) as KVItem[];
+
+            const parsedResumes = resumes?.map((resume) => (
+                JSON.parse(resume.value) as Resume
+            ))
+
+            setResumes(parsedResumes || []);
+            setLoadingResumes(false);
+        }
+
+        loadResumes()
+    }, []);
 
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
 
@@ -30,17 +49,32 @@ export default function Home() {
 
     <section className="main-section">
         <div className="page-heading py-16">
-            <h1>Track Your Applications and Resume Ratings.</h1>
-            <h2>Review your submission & check AI powered feedback</h2>
+            <h1>Track Your Applications & Resume Ratings</h1>
+            {!loadingResumes && resumes?.length === 0 ? (
+                <h2>No resumes found. Upload your first resume to get feedback.</h2>
+            ): (
+                <h2>Review your submissions and check AI-powered feedback.</h2>
+            )}
         </div>
+        {loadingResumes && (
+            <div className="flex flex-col items-center justify-center">
+                <img src="/images/resume-scan-2.gif" className="w-[200px]" />
+            </div>
+        )}
 
-        {resumes.length > 0 && (
+        {!loadingResumes && resumes.length > 0 && (
             <div className="resumes-section">
-                {resumes.map((resume, index) => (
-                    <div>
-                        <ResumeCard key={resume.id} resume={resume}/>
-                    </div>
+                {resumes.map((resume) => (
+                    <ResumeCard key={resume.id} resume={resume} />
                 ))}
+            </div>
+        )}
+
+        {!loadingResumes && resumes?.length === 0 && (
+            <div className="flex flex-col items-center justify-center mt-10 gap-4">
+                <Link to="/upload" className="primary-button w-fit text-xl font-semibold">
+                    Upload Resume
+                </Link>
             </div>
         )}
     </section>
